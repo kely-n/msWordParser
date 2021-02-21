ed6f97b69528ffe8e0c239a28c2f2c97943a82af
kely-n <ukirukely@gmail.com>
2021-02-21 23:17:30 +0300

    lil mess here

--- src/main/java/ParseDocument.java
+++ src/main/java/ParseDocument.java
@@ -1,13 +1,15 @@
 
 /* Dev Kelyn created the file on 2021-02-21 inside the package - PACKAGE_NAME */
 
-import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.xwpf.usermodel.IBodyElement;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
 
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  *
@@ -15,20 +17,28 @@ import java.util.Arrays;
 public class ParseDocument {
     public static void main(String[] args) throws IOException {
 
-        String fileName = "/home/sheis/Downloads/Sample question.docx";
+        String fileName = "/home/sheis/Downloads/Sample.docx";
+            ///home/sheis/Downloads/SOCIEDADE_NACIONAL_-Sample3.docx
+            ///home/sheis/Documents/example.docx
 
-        try (XWPFDocument doc = new XWPFDocument(
-                Files.newInputStream(Paths.get(fileName)))) {
+        int j = 0;
+        try {
+            FileInputStream fis = new FileInputStream(fileName);
+            XWPFDocument xdoc =new XWPFDocument(OPCPackage.open(fis));
+            Iterator<XWPFTable> tableElementIterator = xdoc.getTablesIterator();
 
-            XWPFWordExtractor xwpfWordExtractor = new XWPFWordExtractor(doc);
-            String docText = xwpfWordExtractor.getText();
-            System.out.println(docText);
+            while(tableElementIterator.hasNext()) {
+                j++;
+                System.out.println();
+                XWPFTable table = tableElementIterator.next();
+                System.out.println("table: " +j + " - " +table.getNumberOfRows()+ " Rows");
+                System.out.println(table.getText());
 
-            // find number of words in the document
-            long count = Arrays.stream(docText.split("\\s+")).count();
-            System.out.println("Total words: " + count);
+            }
 
+        } catch(Exception ex) {
+            ex.printStackTrace();
         }
-
+        System.out.println("looped " + j +" times");
     }
 }
